410dfe951f66c44306033801a4aca48ef24eab9c
Pedro Candeias <pgscandeias@gmail.com>
2017-11-28 19:42:28 +0100

    Relative asset paths

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.[hash].js'
+    filename: 'app.[hash].js',
+    publicPath: './'
   },
   module: {
     rules: [
