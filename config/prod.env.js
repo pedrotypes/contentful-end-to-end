f378b1b279b523ee8a093d4a59006e77da259270
Pedro Candeias <pgscandeias@gmail.com>
2017-11-28 20:00:35 +0100

    Fix redirect url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   CONTENTFUL_APP_ID: '"385bc6ca3775ee9f48bdd159eb3c68179f11fc02dfadc64f6ae1a896cc3ec287"',
-  CONTENTFUL_APP_REDIRECT_URL: '"https://pgscandeias.github.io/contentful-end-to-end"',
+  CONTENTFUL_APP_REDIRECT_URL: '"https://pgscandeias.github.io/contentful-end-to-end/oauth/contentful_redirect"',
   CONTENTFUL_CMA_URL: '"https://api.contentful.com/"'
 }
