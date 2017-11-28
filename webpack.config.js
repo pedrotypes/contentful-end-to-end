aef140830e780db649e7325205b298f1b66409dc
Pedro Candeias <pgscandeias@gmail.com>
2017-11-28 19:18:10 +0100

    Tweaks

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path')
+var vueLoaderConfig = require('./vue-loader.conf')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
